a97f156377acc1b93d8eaab7d7e00f9641877dc1
belerico <belo.fede@outlook.com>
2024-02-06 16:49:43 +0000

    Change default lr

--- run_hybrid_table_lm_finetuning.py
+++ run_hybrid_table_lm_finetuning.py
@@ -788,7 +788,7 @@ def main():
         default=1,
         help="Number of updates steps to accumulate before performing a backward/update pass.",
     )
-    parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
+    parser.add_argument("--learning_rate", default=1e-4, type=float, help="The initial learning rate for Adam.")
     parser.add_argument("--linear_scale_lr", action="store_true", help="Whether to linearly scale learning rate.")
     parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight deay if we apply some.")
     parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
